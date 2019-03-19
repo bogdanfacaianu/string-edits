5cb3a39a235e047fdd29eeee570839d6034130c1
bogdanfacaianu <bogdy.facaianu@gmail.com>
2019-03-19 12:18:08 +0000

    things, just things

--- TestInterface/src/main/java/com/stringedits/testrunner/controller/view/LanguageViewController.java
+++ TestInterface/src/main/java/com/stringedits/testrunner/controller/view/LanguageViewController.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/load")
@@ -27,51 +27,60 @@ public class LanguageViewController {
     }
 
     @PostMapping("/loadEnglishWords")
-    public String loadEnglishWordsFromFile() throws FileNotFoundException {
-        return loadLanguage("english");
+    public String loadEnglishWordsFromFile(RedirectAttributes redirect) throws FileNotFoundException {
+        return loadLanguage("english", redirect);
     }
 
     @PostMapping("/loadDictionary")
-    public String loadEnglishWordsFromFile(@RequestParam("dictionaryName") String dictionaryName) throws FileNotFoundException {
-        return loadLanguage(dictionaryName);
+    public String loadEnglishWordsFromFile(
+        @RequestParam("dictionaryName") String dictionaryName,
+        RedirectAttributes redirect) throws FileNotFoundException {
+        return loadLanguage(dictionaryName, redirect);
     }
 
     @PostMapping("/createLanguage")
-    public String createLanguage(@RequestParam("languageName") String languageName) {
+    public String createLanguage(
+        @RequestParam("languageName") String languageName,
+        RedirectAttributes redirect) {
+
         dictionaryService.saveLanguage(new Language(languageName));
+        redirect.addFlashAttribute("flashSuccess", String.format("Language \"%s\" created", languageName));
         return "redirect:/load";
     }
 
     @PostMapping("/addWordFromLanguage")
     public String addWord(
         @RequestParam("languageName") String languageName,
-        @RequestParam("word") String word) {
+        @RequestParam("word") String word,
+        RedirectAttributes redirect) {
 
         dictionaryService.addWordToLanguage(languageName, word);
+        redirect.addFlashAttribute("flashSuccess", String.format("Added %s word in language \"%s\"", word, languageName));
         return "redirect:/load";
-    }
-
-    @GetMapping("/listLanguage")
-    public String getLanguage(@RequestParam("languageName") String languageName) {
-        return "redirect:/listLanguage/" + languageName;
     }
 
     @PostMapping("/removeWordFromLanguage")
     public String removeLanguage(
         @RequestParam("word") String word,
-        @RequestParam("languageName") String languageName) {
+        @RequestParam("languageName") String languageName,
+        RedirectAttributes redirect) {
 
         dictionaryService.removeWordFromLanguage(languageName, word);
+        redirect.addFlashAttribute("flashSuccess", String.format("Removed %s word from language \"%s\"", word, languageName));
         return "redirect:/load";
     }
 
     @PostMapping("/removeLanguage")
-    public String removeLanguage(@RequestParam("languageName") String languageName) {
+    public String removeLanguage(
+        @RequestParam("languageName") String languageName,
+        RedirectAttributes redirect) {
+
         dictionaryService.removeLanguage(languageName);
+        redirect.addFlashAttribute("flashSuccess", String.format("Language \"%s\" removed", languageName));
         return "redirect:/load";
     }
 
-    private String loadLanguage(String name) throws FileNotFoundException {
+    private String loadLanguage(String name, RedirectAttributes redirect) throws FileNotFoundException {
         String path = Objects.requireNonNull(
             getClass().getClassLoader().getResource("dictionary/" + name + ".txt")).getPath();
         Scanner file = new Scanner(new File(path));
@@ -83,6 +92,7 @@ public class LanguageViewController {
 
         Language language = new Language(name, words);
         dictionaryService.saveLanguage(language);
+        redirect.addFlashAttribute("flashSuccess", String.format("Language \"%s\" loaded", name));
         return "redirect:/load";
     }
 }
